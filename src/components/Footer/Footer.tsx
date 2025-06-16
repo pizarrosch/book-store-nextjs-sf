165df0e364e3714a8b76bd10f475044abba215eb
pizarrosch <bayernsch89@gmail.com>
2025-06-17 01:19:57 +0200

    Style Footer component and structure with CSS modules
    Applied CSS modules to the Footer component for consistent styling and better structure. Added dedicated styles for layout alignment, spacing, and background, including specific styles for options and payment sections.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,15 +4,16 @@ import gpay from '../../../public/assets/footer_images/gpay.png';
 import mastercard from '../../../public/assets/footer_images/mastercard.png';
 import paypal from '../../../public/assets/footer_images/paypal.png';
 import visa from '../../../public/assets/footer_images/visa.png';
+import s from './Footer.module.scss';
 
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <span>Free returns</span>
-        <span>In-store pickup</span>
+    <div className={s.root}>
+      <div className={s['options-container']}>
+        <span>&#10003; Free returns</span>
+        <span>&#10003; In-store pickup</span>
       </div>
-      <div>
+      <div className={s['payment-container']}>
         <Image src={visa} alt="visa" />
         <Image src={mastercard} alt="mastercard" />
         <Image src={gpay} alt="gpay" />
